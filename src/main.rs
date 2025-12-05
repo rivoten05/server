1c476ed0b1bba490dec1f1bbf5ac639bb3415f98
hitachi-payments <hitachi-payments@gmail.com>
2025-12-05 13:42:58 +0530

    add tcplistener

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use server::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed To bind random port.");
+
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
 use server::run;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,8 +17,12 @@ async fn health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("Failed to run server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed To bind random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = run(listener).expect("Failed to run server");
 
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
